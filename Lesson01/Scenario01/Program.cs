9674263085abdce6217592da0cafbb47c1bac647
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-11-21 23:20:01 -0200

    Lesson01

---
+++ Lesson01/Scenario01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Scenario01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the quantity of products you will buy (numbers only): ");
+                string input = Console.ReadLine();
+                try
+                {                    
+                    int quantity = int.Parse(input);
+                    if (quantity == 0)
+                    {
+                        Console.WriteLine($"Please, enter a quantity greater than zero.");
+                        PressKeyToContinue();
+                        continue;
+                    }
+
+                    int discount = Discount(quantity);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    PressKeyToContinue();
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine($"The number {input} is too big to an integer type.");
+                    Console.WriteLine($"Please type a number less than {int.MaxValue}.");
+                    PressKeyToContinue();
+                }                
+            }
+        }
+
+        static void PressKeyToContinue()
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static int Discount(int quantity)
+        {
+            if (quantity < 10)
+                return 5;
+
+            if (quantity < 50)
+                return 10;
+
+            if (quantity < 100)
+                return 15;
+
+            return 0;
+        }
+    }
+}
---
+++ Lesson01/Scenario02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Scenario02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input N: ");
+            int n  = int.Parse(Console.ReadLine());
+            int fact = 1;
+            while (n > 1)
+            {
+                fact = fact * n;
+                n--;
+            }
+
+            Console.WriteLine($"Factorial is: {fact}");
+
+            Console.ReadKey();
+        }
+    }
+}
